66285afb4c49d63de1d26c7939b2d231ec0feb7d
Tyler Gaw <me@tylergaw.com>
2015-01-08 17:03:50 -0500

    Adding the useDefaultCSS option for the time being

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,11 @@
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-var app = new EmberAddon();
+var app = new EmberAddon({
+  'ember-cli-chartist': {
+    'useDefaultCSS': true
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
