93b97da7edca61c6bb8add259bcd2ca04d08aaae
Tyler Gaw <me@tylergaw.com>
2015-01-08 17:03:29 -0500

    Adding chartist scss

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function () {
+    return {
+      chartData: {
+        labels: ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6'],
+        series: [
+          [5, 4, 3, 7, 5, 10],
+          [3, 2, 9, 5, 4, 6],
+          [2, 1, -3, -4, -2, 0]
+        ]
+      }
+    };
+  }
+});
