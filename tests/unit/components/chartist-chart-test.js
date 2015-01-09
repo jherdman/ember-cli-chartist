31706f13f603f0e7889009846adffd65e8c632e7
Tyler Gaw <me@tylergaw.com>
2015-01-09 00:57:17 -0500

    Adds a first round of tests

--- tests/unit/components/chartist-chart-test.js
+++ tests/unit/components/chartist-chart-test.js
@@ -1,21 +1,119 @@
+/* global Ember */
+
 import {
   moduleForComponent,
   test
 } from 'ember-qunit';
 
-moduleForComponent('chartist-chart', 'ChartistChartComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
-});
+moduleForComponent('chartist-chart', 'ChartistChartComponent', {});
+
+var chartData = {
+    labels: ['Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6'],
+    series: [
+      [5, 4, 3, 7, 5, 10],
+      [3, 2, 9, 5, 4, 6],
+      [2, 1, -3, -4, -2, 0]
+    ]
+  },
+  pieChartData = {
+    labels: ['Pizza', 'Fish', 'Puppies'],
+    series: [
+      [40, 25, 25]
+    ]
+  };
 
 test('it renders', function() {
   expect(2);
 
-  // creates the component instance
-  var component = this.subject();
+  var component = this.subject({
+    data: chartData
+  });
+
   equal(component._state, 'preRender');
 
-  // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+test('it can be a line chart', function() {
+  expect(1);
+
+  var component = this.subject({
+    data: chartData
+  });
+
+  component.set('type', 'line');
+  this.append();
+
+  var chart = component.get('chart');
+  equal(chart.options.classNames.chart, 'ct-chart-line');
+});
+
+test('it can be a bar chart', function() {
+  expect(1);
+
+  var component = this.subject({
+    data: chartData
+  });
+
+  component.set('type', 'bar');
+  this.append();
+
+  var chart = component.get('chart');
+  equal(chart.options.classNames.chart, 'ct-chart-bar');
+});
+
+test('it can be a pie chart', function() {
+  expect(1);
+
+  var component = this.subject({
+    data: pieChartData
+  });
+
+  component.set('type', 'pie');
+  this.append();
+
+  var chart = component.get('chart');
+  equal(chart.options.classNames.chart, 'ct-chart-pie');
+});
+
+test('it can have different ratios', function () {
+  expect(1);
+
+  var component = this.subject({
+    data: chartData
+  });
+
+  var ratio = 'ct-minor-second';
+  component.set('ratio', ratio);
+  ok(this.$().hasClass(ratio));
+});
+
+test('it can be configured with the options attribute', function () {
+  expect(4);
+
+  var component = this.subject({
+    data: chartData
+  });
+
+  component.set('options', {
+    showArea: false,
+    lineSmooth: false,
+    axisX: {
+      showGrid: false
+    },
+    axisY: {
+      showLabel: false
+    }
+  });
+
+  this.append();
+
+  var chart = component.get('chart');
+  var opts = chart.options;
+
+  equal(opts.showArea, false);
+  equal(opts.lineSmooth, false);
+  equal(opts.axisX.showGrid, false);
+  equal(opts.axisY.showLabel, false);
 });
