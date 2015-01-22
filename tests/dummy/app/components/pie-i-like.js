71c69bcc50a043d92751d86073ffc200503dd70e
Tyler Gaw <me@tylergaw.com>
2015-01-21 23:36:36 -0500

    Simplyfy the pie chart exteneded example

--- tests/dummy/app/components/pie-i-like.js
+++ tests/dummy/app/components/pie-i-like.js
@@ -1,25 +1,9 @@
 import ChartistChart from './chartist-chart';
 
-function getData () {
-  return {
-    series: [
-      ['Chocolate', 'Apple', 'Steak']
-    ]
-  };
-}
-
-var pieChartData = {
-  labels: ['Pizza', 'Fish', 'Puppies'],
-  series: [
-    [40, 25, 25]
-  ]
-}
-
 export default ChartistChart.extend({
   type: 'pie',
-  data: pieChartData
-  // init: function () {
-  //   this.set('data', pieChartData);
-  //   this._super();
-  // }
+  data: {
+    labels: ['Chocolate', 'Apple', 'Pumpkin'],
+    series: [30, 15, 65]
+  }
 });
