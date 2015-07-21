d19b09df5885a9bfcbb36ecb1c223489d585ce3c
Tyler Gaw <me@tylergaw.com>
2015-07-21 16:00:39 -0400

    Clean up blueprint a bit

--- blueprints/ember-cli-chartist/index.js
+++ blueprints/ember-cli-chartist/index.js
@@ -1,5 +1,11 @@
+/* jshint node: true */
+'use strict';
+
 module.exports = {
   normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
   },
 
   beforeInstall: function(options) {
