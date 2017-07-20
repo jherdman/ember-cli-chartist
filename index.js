a161e83820f44d9f827bace7752dc80588e047d6
Benjamin Lodi <brlodi@gmail.com>
2017-07-20 16:30:36 -0500

    Implement treeForStyles function

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
+
   name: 'ember-cli-chartist',
 
   treeForVendor(vendorTree) {
@@ -18,51 +19,23 @@ module.exports = {
       ],
     });
 
-    var chartistTreeSCSS = new Funnel(chartistPath, {
-      srcDir: 'scss',
-
-      include: [
-        '*.scss',
-      ],
-    });
-
-    return new MergeTrees([
+    return mergeTrees([
       vendorTree,
       chartistTree,
-      chartistTreeSCSS,
     ]);
+  },
+
+  treeForStyles() {
+    var chartistPath = path.dirname(require.resolve('chartist'));
+
+    return new Funnel(chartistPath, {
+      srcDir: 'scss',
+    });
   },
 
   included(app) {
     this._super.included.apply(this, arguments);
-
     app.import('vendor/chartist.js');
-    app.import('vendor/chartist.css');
-    app.import('vendor/chartist.scss');
   },
 
-  //included: function included(app, parentAddon) {
-  //  var target = (parentAddon || app);
-
-  //  var options = target.options['ember-cli-chartist'] || {};
-  //  var chartistPath = path.join(target.bowerDirectory, 'chartist', 'dist');
-
-  //  if (options.useCustomCSS) {
-  //    target.options.sassOptions = target.options.sassOptions || {};
-  //    target.options.sassOptions.includePaths = target.options.sassOptions.includePaths || [];
-
-  //    target.options.sassOptions.includePaths.push(
-  //      path.join(chartistPath, 'scss')
-  //    );
-
-  //    target.options.sassOptions.includePaths.push(
-  //      path.join(chartistPath, 'scss', 'settings')
-  //    );
-
-  //  } else {
-  //    target.import(path.join(chartistPath, 'chartist.min.css'));
-  //  }
-
-  //  app.import(path.join(chartistPath, 'chartist.js'));
-  //}
 };
