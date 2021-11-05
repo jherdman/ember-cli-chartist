992aad30791ae897af829361a76e3178ec820c7f
James Herdman <james.herdman@gmail.com>
2021-11-05 09:17:37 -0400

    Resolve some deprecation warnings

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,7 +1,11 @@
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
+  @tracked
+  deferredComponentData = null;
+
   chartResOptions = [
     [
       'screen and (min-width: 640px)',
@@ -16,22 +20,27 @@ export default class ApplicationController extends Controller {
     ],
   ];
 
+  pieData = {
+    labels: ['Chocolate', 'Apple', 'Pumpkin'],
+    series: [30, 15, 65],
+  };
+
   @task
   *loadAsyncData() {
     const labels = ['Salmon', 'Yellowtail', 'Dolphin', 'Cow'];
 
     yield timeout(500);
 
-    this.set('deferredComponentData', {
+    this.deferredComponentData = {
       labels,
       series: [['2', '8', '3', '9']],
-    });
+    };
 
     yield timeout(4000);
 
-    this.set('deferredComponentData', {
+    this.deferredComponentData = {
       labels,
       series: [['22', '18', '0', '19']],
-    });
+    };
   }
 }
